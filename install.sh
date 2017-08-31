7008a41ba3cc22900a1e983ab49e8604143bd1fb
User <jacob.zipper@pinecrest.edu>
2017-08-31 14:22:42 -0400

    new links

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl "https://doc-10-ac-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/r79gcld8v5oif8hvnv0lt38guuo8kcic/1504137600000/05231677468164863915/*/0BxwJN5PnmVXwWi1ybVl5d1JwYlU?e=download" -o /usr/local/bin/checkstyle.jar
-curl "https://doc-10-ac-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/fo1i61oucvemfhjko7kv2h0fh8sb2v5j/1504152000000/05231677468164863915/*/0BxwJN5PnmVXwTlhnTU9mcGFKcEU?e=download" -o /usr/local/bin/checkstyle
+curl "https://www.dl.dropboxusercontent.com/s/btp4s5l9i6xvq3z/checkstyle.jar" -o /usr/local/bin/checkstyle.jar
+curl "https://www.dl.dropboxusercontent.com/s/ujjwj65gttqid2e/checkstyle.sh" -o /usr/local/bin/checkstyle
 chmod a+x /usr/local/bin/checkstyle
 chmod a+x /usr/local/bin/checkstyle.jar
