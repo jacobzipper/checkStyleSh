92597bbfc4f3df9a9f77e0bca105b075d8bbab75
User <jacob.zipper@pinecrest.edu>
2017-08-31 00:06:04 -0400

    args option

--- checkstyle.sh
+++ checkstyle.sh
@@ -1,22 +1,51 @@
-checkstyle=~/checkstyle.jar
-
+checkstyle=/usr/local/bin/checkstyle.jar
+count=0
 if (($# < 1)); then
-    echo -e "\033[1;31mERROR: \033[0;31mNo argument found\033[0;0m"
-    exit
+	echo -e "\033[1;31mERROR: \033[0;31mNo argument found\033[0;0m"
+	exit
 fi
-for var in "$@"
-do
-	file=$var
+while getopts ":a" opt; do
+	case $opt in
+		a)
+			shift
+			count=$((count + 1))
+			for var in "$@"
+			do
+				file=$var
+				echo -e "\033[1;31mCompiling\033[0;0m"
+				filename=${file##*/}
+				filenameNoExtension=${filename%.*}
+				dirpath=${file%/*}
+				cd $dirpath
+				if javac $filename; then
+					printf "Compiled Successfully\n\n"
+				else
+					echo "Failed to compile"
+					continue
+				fi
+				echo -e "\033[1;31mRunning Checkstyle\033[0;0m"
+				java -jar $checkstyle $filename
+				printf "\n"
+				echo -e "\033[1;31mRunning program\033[0;0m"
+				shift #Get rid of the first parameter
+				java $filenameNoExtension
+			done
+			;;
+	esac
+done
+if [ $count -eq 0 ]
+then
+	file=$1
 	echo -e "\033[1;31mCompiling\033[0;0m"
 	filename=${file##*/}
 	filenameNoExtension=${filename%.*}
 	dirpath=${file%/*}
 	cd $dirpath
 	if javac $filename; then
-		printf "Compiled Successfully\n\n"
+	    printf "Compiled Successfully\n\n"
 	else
-		echo "Failed to compile"
-		continue
+	    echo "Failed to compile"
+	    exit
 	fi
 	echo -e "\033[1;31mRunning Checkstyle\033[0;0m"
 	java -jar $checkstyle $filename
@@ -24,4 +53,4 @@ do
 	echo -e "\033[1;31mRunning program\033[0;0m"
 	shift #Get rid of the first parameter
 	java $filenameNoExtension $@
-done
+fi
